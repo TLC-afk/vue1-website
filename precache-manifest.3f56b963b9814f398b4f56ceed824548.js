0ddf07456acdca7fa825e908a8d5925a473e7d60
Mr.tang <1261651023@qq.com>
2021-09-17 21:43:04 +0800

    deploy

---
+++ precache-manifest.3f56b963b9814f398b4f56ceed824548.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ab53778229748792f7",
+    "url": "/vue1-website/css/app.8e506cfd.css"
+  },
+  {
+    "revision": "173a9b84c585a8713c0ac19f4660383d",
+    "url": "/vue1-website/index.html"
+  },
+  {
+    "revision": "62ab53778229748792f7",
+    "url": "/vue1-website/js/app.fbf9270f.js"
+  },
+  {
+    "revision": "b5cfed99ea2570cb0408",
+    "url": "/vue1-website/js/chunk-vendors.ef8eb22a.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/vue1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue1-website/robots.txt"
+  }
+]);
